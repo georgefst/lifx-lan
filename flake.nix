49a4a93a38a2cf3a15bbbb05d92caa0fbb7613b1
George Thomas <georgefsthomas@gmail.com>
2023-07-23 18:25:55 +0100

    Switch to ARM

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   outputs = { self, nixpkgs }:
     let
       ghcVersion = "ghc927";
-      system = "x86_64-linux";
+      system = "aarch64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       hpkgs = pkgs.haskell.packages.${ghcVersion};
       lifx-lan = hpkgs.callCabal2nix "lifx-lan" ./. { };
