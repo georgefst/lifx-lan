ee976df124125cacc7a705a781814a2091e734ef
George Thomas <georgefsthomas@gmail.com>
2023-07-23 18:25:28 +0100

    Add cabal2nix flake
    Essentially from https://discourse.nixos.org/t/cabal2nix-haskell-flake-doesnt-recognise-the-moved-or-renamed-cabal-file/30588. I don't quite know how they actually generated it.

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+  outputs = { self, nixpkgs }:
+    let
+      ghcVersion = "ghc927";
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      hpkgs = pkgs.haskell.packages.${ghcVersion};
+      lifx-lan = hpkgs.callCabal2nix "lifx-lan" ./. { };
+      tools = with hpkgs; [ cabal-install ];
+    in
+    {
+      packages.${system}.default = lifx-lan;
+      devShells.${system}.default = hpkgs.shellFor {
+        packages = p: [ lifx-lan ];
+        nativeBuildInputs = tools; #++ [ pkgs.zlib ];
+      };
+    };
+}
